1f929d3c3e5f446372a2619ba7b67310b1a1cb23
Shubham Tiwari <thegimreper@gmail.com>
2020-06-22 07:34:07 -0400

    yolo

---
+++ build/precache-manifest.8d0a087e4ed135bd95abfc369b7e66bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e399c407475df3b7dc27b27900466a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0522beffbee1a8c6440",
+    "url": "/static/css/main.a240174b.chunk.css"
+  },
+  {
+    "revision": "ea24a2247e1e674524e8",
+    "url": "/static/js/2.73b7de97.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.73b7de97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0522beffbee1a8c6440",
+    "url": "/static/js/main.8c918c0b.chunk.js"
+  },
+  {
+    "revision": "dd16d6b254755b2fc4bb",
+    "url": "/static/js/runtime-main.3adb91a3.js"
+  }
+]);
